242ac15bb5673dc755c9cdcca62101d8d9a4f535
mengxiangchu <mengxiangchu@houbank.com>
2018-07-04 14:56:08 +0800

    version 1.1

--- GitPro1/src/GitTest.java
+++ GitPro1/src/GitTest.java
@@ -3,5 +3,6 @@ public class GitTest {
 public static void main(String[] args) {
 	System.out.println("changed1");
 	System.out.println("this is a git test");
+	System.out.println("now ,i have changed again!");
 }
 }
